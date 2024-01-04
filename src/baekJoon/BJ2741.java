1fd67625c478db5103896dd23751e8edfe450fb6
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-01-04 20:58:27 +0900

    [BJ 2739, 2741]

---
+++ src/baekJoon/BJ2741.java
@@ -0,0 +1,21 @@
+package baekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import static java.lang.Integer.parseInt;
+
+public class BJ2741 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int n = parseInt(bufferedReader.readLine());
+
+        for (int i = 1; i <= n; i++) {
+
+            System.out.println(i);
+        }
+    }
+}
