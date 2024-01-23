f2cd867a647f11e70ec9f9f8b872caedb107a1ff
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-01-23 21:52:22 +0900

    [BJ 15654]
    dfs
    bw

--- src/baekJoon/BJ15654.java
+++ src/baekJoon/BJ15654.java
@@ -1,48 +1,42 @@
 package baekJoon;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Arrays;
-
-import static java.lang.Integer.parseInt;
 
 public class BJ15654 {
 
-    static StringBuilder stringBuilder = new StringBuilder();
     static int n, m;
     static int[] numbers;
     static boolean[] visited;
-
+    static BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
 
     public static void main(String[] args) throws IOException {
-
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         String[] nm = bufferedReader.readLine().split(" ");
-        n = parseInt(nm[0]);
-        m = parseInt(nm[1]);
+        n = Integer.parseInt(nm[0]);
+        m = Integer.parseInt(nm[1]);
 
         numbers = Arrays.stream(bufferedReader.readLine().split(" ")).mapToInt(Integer::parseInt).sorted().toArray();
         visited = new boolean[n];
-        dfs(0);
-
+        dfs(0, "");
+        bufferedWriter.flush();
     }
 
-    private static void dfs(int depth) {
-
+    private static void dfs(int depth, String result) throws IOException {
         if (depth == m) {
-            System.out.println(stringBuilder.toString().trim());
+            bufferedWriter.write(result.trim() + "\n");
             return;
         }
 
         for (int i = 0; i < n; i++) {
             if (!visited[i]) {
                 visited[i] = true;
-                stringBuilder.append(numbers[i]).append(" ");
-                dfs(depth + 1);
+                dfs(depth + 1, result + numbers[i] + " ");
                 visited[i] = false;
-                stringBuilder.delete(stringBuilder.lastIndexOf(String.valueOf(numbers[i])), stringBuilder.length());
-
             }
         }
     }
