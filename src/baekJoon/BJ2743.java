adbe52a4dac7e157833f6d10e74e9f10e2593c2d
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-03-15 22:57:11 +0900

    [BJ 2743]
    오늘은 프젝해야해...

---
+++ src/baekJoon/BJ2743.java
@@ -0,0 +1,14 @@
+package baekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BJ2743 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(bufferedReader.readLine().length());
+    }
+}
