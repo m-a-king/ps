54829c7b035fd43f8683468999ec9f7ef2c7efd1
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-03-29 21:23:28 +0900

    [BJ 10718]

---
+++ src/baekJoon/BJ10718.java
@@ -0,0 +1,9 @@
+package baekJoon;
+
+public class BJ10718 {
+
+    public static void main(String[] args) {
+        System.out.println("강한친구 대한육군");
+        System.out.println("강한친구 대한육군");
+    }
+}
