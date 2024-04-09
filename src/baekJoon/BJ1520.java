66cc97d4c254ffce1bfa6a7cbaf9fbcc6d9f9f69
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-04-09 17:15:58 +0900

    [BJ 1520]
    주석 추가

--- src/baekJoon/BJ1520.java
+++ src/baekJoon/BJ1520.java
@@ -3,13 +3,10 @@ package baekJoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
 import java.util.PriorityQueue;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class BJ1520 {
-
 
     private static class Pos implements Comparable<Pos> {
         int m, n;
@@ -19,6 +16,7 @@ public class BJ1520 {
             this.n = n;
         }
 
+        // 높이를 기준으로 내림차순으로 정렬
         @Override
         public int compareTo(Pos o) {
             return map[o.m][o.n] - map[this.m][this.n];
@@ -27,7 +25,7 @@ public class BJ1520 {
 
     static int[] dm = {1, -1, 0, 0};
     static int[] dn = {0, 0, 1, -1};
-    static int n, m;
+    static int m, n;
     static int[][] map;
 
     public static void main(String[] args) throws IOException {
@@ -35,6 +33,7 @@ public class BJ1520 {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine(), " ");
 
+        // 세로 m, 가로 n 입력
         m = Integer.parseInt(stringTokenizer.nextToken());
         n = Integer.parseInt(stringTokenizer.nextToken());
 
@@ -47,10 +46,14 @@ public class BJ1520 {
             }
         }
 
+        // 우선순위 큐 사용 (내림차순, 최대 힙)
         PriorityQueue<Pos> queue = new PriorityQueue<>();
         queue.offer(new Pos(0, 0));
 
+        // 해당 지점까지 이동할 수 있는 경로의 수를 저장
+        // visited 배열의 역할도 가짐
         int[][] dp = new int[m][n];
+        // 시작 지점으로 가는 방법은 처음 시작할 때, 한 번
         dp[0][0] = 1;
 
         while (!queue.isEmpty()) {
@@ -61,24 +64,30 @@ public class BJ1520 {
                 int nextM = current.m + dm[d];
                 int nextN = current.n + dn[d];
 
+                // 배열 범위 검사
                 if (!isSafe(nextM, nextN)) {
                     continue;
                 }
+                // 다음 지점으로 이동 가능한지 확인 (높이 체크)
                 if (currentHeight <= map[nextM][nextN]) {
                     continue;
                 }
+                // 해당 지점에 처음 왔다면?
                 if (dp[nextM][nextN] == 0) {
+                    // 큐에 삽입하고 아래 식으로
                     queue.offer(new Pos(nextM, nextN));
                 }
+                // 해당 지점으로 올 수 있는 경로의 수 = (1) + (2)
+                // (1) 이전에 계산된 해당 지점으로 올 수 있는 경로의 수
+                // (2) 해당 지점으로 올 수 있는 이전 지점으로 올 수 있는 경로의 수
                 dp[nextM][nextN] += dp[current.m][current.n];
 
             }
         }
+
+        // 결과 출력
         System.out.println(dp[m - 1][n - 1]);
-
-
     }
-
 
 
     private static boolean isSafe(int nextM, int nextN) {
