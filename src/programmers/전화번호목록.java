f5832eb9f9df2d8ba0b11c21cd339726a96e01a2
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-04-30 16:31:55 +0900

    [P 전화번호목록]

---
+++ src/programmers/전화번호목록.java
@@ -0,0 +1,23 @@
+package programmers;
+
+import java.util.Arrays;
+
+public class 전화번호목록 {
+
+    class Solution {
+        public boolean solution(String[] phone_book) {
+            boolean answer = true;
+
+            Arrays.sort(phone_book);
+
+            for (int i = 0; i < phone_book.length - 1; i++) {
+                if (phone_book[i + 1].startsWith(phone_book[i])) {
+                    answer = false;
+                    break;
+                }
+            }
+
+            return answer;
+        }
+    }
+}
