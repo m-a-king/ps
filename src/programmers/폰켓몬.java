66f3576c22b1d7e1a9150ad57a3d88a86851d4b8
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-04-30 15:44:18 +0900

    [P 폰켓몬]

---
+++ src/programmers/폰켓몬.java
@@ -0,0 +1,26 @@
+package programmers;
+
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class 폰켓몬 {
+
+    public static void main(String[] args) {
+        class Solution {
+            public int solution(int[] nums) {
+                int answer = 0;
+
+                int arrlength = nums.length;
+
+                // nums 배열을 스트림으로 변환하고, boxed()로 IntStream을 Stream<Integer>로 변환한 후, Set으로 수집
+                Set<Integer> set = Arrays.stream(nums).boxed().collect(Collectors.toSet());
+
+                int setLength = set.size();
+
+                answer = Math.min(arrlength / 2, setLength);
+                return answer;
+            }
+        }
+    }
+}
