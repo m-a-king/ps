9f07136b29e58c1b926b066ba5dd8b1772bbf9aa
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-04-30 15:42:56 +0900

    [BJ 7287]

---
+++ src/baekJoon/BJ7287.java
@@ -0,0 +1,12 @@
+package baekJoon;
+
+import java.io.IOException;
+
+public class BJ7287 {
+
+    public static void main(String[] args) throws IOException {
+
+        System.out.println(210);
+        System.out.println("qwe3492");
+    }
+}
