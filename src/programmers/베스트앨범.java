7b370b5643b5a87f60bb5f1d5ed93443c1a8c832
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-05-01 16:04:10 +0900

    [P 베스트앨범] 해시맵

--- src/programmers/베스트앨범.java
+++ src/programmers/베스트앨범.java
@@ -7,46 +7,71 @@ public class 베스트앨범 {
 
     class Solution {
 
-        private static class Genre {
-            int idx, plays;
+        // 각 노래의 입력 순서와 재생 횟수를 표현할 수 있는 클래스 정의
+        private static class Song implements Comparable<Song>{
+            int idx, play;
 
-            public Genre(int idx, int plays) {
+            public Song(int idx, int play) {
                 this.idx = idx;
-                this.plays = plays;
+                this.play = play;
+            }
+
+            // 우선 순위 큐 사용을 위한 비교 메서드 정의 (내림차순)
+            @Override
+            public int compareTo(Song o) {
+                return o.play - this.play;
             }
         }
 
         public int[] solution(String[] genres, int[] plays) {
 
+            // 각 장르의 총 재생 횟수를 저장
             HashMap<String, Integer> accMap = new HashMap<>();
-            HashMap<String, List<Genre>> listMap = new HashMap<>();
+
+            // 각 장르에 포함되는 노래들을 우선 순위 큐를 사용해서 저장
+            HashMap<String, PriorityQueue<Song>> pqMap = new HashMap<>();
+
+            // plays 배열 요소를 순회하기 위한 인덱스
             int idx = 0;
 
             for (String genre : genres) {
+                // 해당 장르에 재생 횟수를 더함
                 accMap.put(genre, accMap.getOrDefault(genre, 0) + plays[idx]);
-                List<Genre> list = listMap.getOrDefault(genre, new ArrayList<>());
-                list.add(new Genre(idx, plays[idx++]));
-                listMap.put(genre, list); // 이 리스트를 다시 맵에 저장합니다.
+
+                // 해당 장르에 Song 객체를 추가함
+                // getOrDefault를 사용했기 때문에 아래와 같이 적으면 안됨 (Default 로 생성되는 pq에서 문제 발생)
+                // (오류 발생) pqMap.getOrDefault(genre, new PriorityQueue<>()).offer(new Song(idx, plays[idx++]);
+                PriorityQueue<Song> pq = pqMap.getOrDefault(genre, new PriorityQueue<>());
+                pq.offer(new Song(idx, plays[idx++]));
+                pqMap.put(genre, pq);
             }
 
+            // 각 장르의 총 재생 횟수를 저장하는 accMap을 Map.Entry<String, Integer> 형태로 리스트로 변환
             List<Map.Entry<String, Integer>> entryList = new ArrayList<>(accMap.entrySet());
+
+            // 리스트의 요소(Entry)들을 총 재생 횟수를 기준으로 내림차순 정렬
             Collections.sort(entryList, (entry1, entry2) -> entry2.getValue().compareTo(entry1.getValue()));
+
+            // 정답 저장 리스트
+            // 배열로 구현시 각 장르에 1개의 곡이 있으면 예외 처리가 필요하므로 리스트로 정답을 저장
             List<Integer> answer = new ArrayList<>();
 
+            // 정렬된 entryList의 요소를 순회
             for (Map.Entry<String, Integer> entry : entryList) {
-                List<Genre> list = listMap.get(entry.getKey());
-                list.sort((a, b) -> b.plays - a.plays);
+                // 해당 장르에 속하는 우선 순위 큐를 가져옴
+                PriorityQueue<Song> pq = pqMap.get(entry.getKey());
 
                 int count = 0;
-                for (Genre genre : list) {
-                    count++;
-                    answer.add(genre.idx);
-                    if (count == 2) {
-                        break;
-                    }
+                // pq가 비어있지 않아야 함 && 각 장르당 노래가 2개를 초과하면 안됨
+                while (!pq.isEmpty() && count++ != 2) {
+                    // 우선 순위 큐이므로 해당 장르의 가장 많은 재생 횟수를 가진 노래부터 dequeue
+                    Song song = pq.poll();
+                    // dequeue 된 노래의 입력 순서를 저장
+                    answer.add(song.idx);
                 }
             }
 
+            // 리스트로 저장된 answer 를 배열로 변경해서 출력 양식을 맞춤.
             return answer.stream().mapToInt(i -> i).toArray();
         }
 
