cb0498dd7fba7c3594fe9a8f0103b879f3d34444
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-05-01 21:04:22 +0900

    [P 프로세스]
    큐, 우선순위큐 주석 추가

--- src/programmers/프로세스.java
+++ src/programmers/프로세스.java
@@ -1,13 +1,13 @@
 package programmers;
 
 import java.util.ArrayDeque;
-import java.util.PriorityQueue;
 import java.util.Queue;
 
 public class 프로세스 {
 
     static class Solution {
 
+        // 해당 프로세스의 우선도와 입력순서를 저장하는 클래스를 정의
         private static class Process {
             int priority, idx;
 
@@ -23,6 +23,7 @@ public class 프로세스 {
 
             Queue<Process> queue = new ArrayDeque<>();
 
+            // 큐에 각 요소들을 입력
             int idx = 0;
             for (int priority : priorities) {
                 queue.offer(new Process(priority, idx++));
@@ -31,10 +32,18 @@ public class 프로세스 {
             while (!queue.isEmpty()) {
                 Process current = queue.poll();
 
+                // 현재 꺼낸 프로세스보다 더 큰 프로세스가 있다면?
                 if (hasHigherPriority(queue, current.priority)) {
+                    // 꺼낸 프로세스를 다시 넣음
                     queue.offer(current);
-                } else {
+                }
+
+                // 현재 꺼낸 프로세스가 제일 큰 프로세스라면?
+                else {
+                    // 해당 프로세스를 (answer+1)순위로 꺼냄
                     answer++;
+                    // 해당 프로세스가 문제에서 요구하는 답이라면?
+                    // 해당 프로세스가 몇 번째 순위로 꺼내졌는지 반환
                     if (current.idx == location) {
                         return answer;
                     }
@@ -43,13 +52,17 @@ public class 프로세스 {
             return answer;
         }
 
+        // 나보다 우선순위가 큰 프로세스가 있는지 검사하는 메서드
         private boolean hasHigherPriority(Queue<Process> queue, int current) {
 
+            // 큐에서 방금 꺼낸 프로세스를 제외한 모든 프로세스를 검사
             for (Process process : queue) {
+                // 우선순위가 더 높은 프로세스가 있다면 true 반환
                 if (process.priority > current) {
                     return true;
                 }
             }
+            // 없다면 false 반환
             return false;
         }
 
--- src/programmers/프로세스우선순위큐.java
+++ src/programmers/프로세스우선순위큐.java
@@ -8,20 +8,32 @@ public class 프로세스우선순위큐 {
 
     public static int solution(int[] priorities, int location) {
         int answer = 0;
-        PriorityQueue<Integer> queue = new PriorityQueue<>(Comparator.reverseOrder());
+
+        // 내림차순으로 정렬되도록 Comparator.reverseOrder()를 사용 (기본적으로 오름차순)
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+
         for (int i = 0; i < priorities.length; i++) {
-            queue.add(priorities[i]);
+            pq.add(priorities[i]);
         }
 
-        while (!queue.isEmpty()) {
+
+        while (!pq.isEmpty()) {
             for (int i = 0; i < priorities.length; i++) {
-                if (queue.peek() == priorities[i]) {
+                // pq의 front가 입력 배열의 몇 번째 요소인지 찾아냄
+                // 그 다음 반복은 해당 idx부터 반복 (반복문 break 없으니, 당연한 것)
+                if (pq.peek() == priorities[i]) {
+                    // 찾아냈다면 순위 갱신 (몇 번째로 실행되는지)
+                    answer++;
+
+                    // 해당 요소가 문제의 요구 사항이라면?
                     if (i == location) {
-                        answer++;
+                        // 정답 반환
                         return answer;
-                    } else {
-                        queue.poll();
-                        answer++;
+                    }
+                    // 해당 요소가 문제의 요구 사항이 아니라면?
+                    else {
+                        // dequeue 하고 다시 for 문으로 이동
+                        pq.poll();
                     }
                 }
             }
