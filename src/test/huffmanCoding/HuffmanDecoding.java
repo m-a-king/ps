77bbd79d4438468c4a8f891964a53c765f68e87f
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-06-03 23:11:46 +0900

    [알고리즘, 허프만 코딩]

---
+++ src/test/huffmanCoding/HuffmanDecoding.java
@@ -0,0 +1,103 @@
+package test.huffmanCoding;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class HuffmanDecoding {
+
+    // 허프만 인코딩을 위한 테이블과 키, 값 위치 변경 (인코딩의 역순)
+    static Map<String, Character> huffmanCodes = new HashMap<>();
+
+    public static void main(String[] args) {
+        String filePath = "/Users/making/IdeaProjects/ps/src/test/huffmanCoding/encodedText.txt";  // 텍스트 파일 경로
+        String text = readFile(filePath);
+        if (text == null) {
+            System.out.println("File content is NULL");
+            return;
+        }
+
+        System.out.println(">>>>>>>>>> ENCODED TEXT START <<<<<<<<<<");
+        System.out.print(text);
+        System.out.println(">>>>>>>>>>> ENCODED TEXT END <<<<<<<<<<<");
+
+        // 헤더와 인코딩된 텍스트 분리
+        String[] parts = text.split("\n", 2);
+        String header = parts[0].substring("header = ".length());
+        String encodedText = parts[1].substring("encodedText = ".length());
+
+        parseHeader(header);
+
+        String decodedText = decodeText(encodedText); // == originalText
+
+        System.out.println(">>>>>>>>>> DECODED TEXT START <<<<<<<<<<");
+        System.out.println(decodedText);
+        System.out.println(">>>>>>>>>>> DECODED TEXT END <<<<<<<<<<<");
+
+        // 디코딩된 텍스트를 파일에 작성
+        String outputFilePath = "/Users/making/IdeaProjects/ps/src/test/huffmanCoding/decodedText.txt"; // 출력 파일 경로 설정
+        writeFile(outputFilePath, decodedText);
+    }
+
+
+    private static String decodeText(String encodedText) {
+        StringBuilder decodedText = new StringBuilder();
+        String tempText = "";
+
+        for (char bit : encodedText.toCharArray()) {
+            tempText += bit;
+
+            if (huffmanCodes.containsKey(tempText)) {
+                decodedText.append(huffmanCodes.get(tempText));
+                tempText = "";
+            }
+        }
+
+        return decodedText.toString();
+    }
+
+    private static void parseHeader(String header) {
+        // 중괄호 제거
+        header = header.substring(1, header.length() - 1);
+
+        // 쉼표를 기준으로 키-값 쌍 분리
+        String[] pairs = header.split(", ");
+
+        for (String pair : pairs) {
+            // 등호를 기준으로 키와 값 분리
+            String[] keyValue = pair.split("=");
+            if (keyValue.length == 2) {
+                // 키는 문자열, 값은 문자로 변환하여 맵에 삽입
+                char key = keyValue[0].charAt(0);
+                String value = keyValue[1];
+                huffmanCodes.put(value, key); // (ex) 010 : a
+            }
+        }
+    }
+
+    private static String readFile(String filePath) {
+        StringBuilder content = new StringBuilder();
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                content.append(line).append("\n");
+            }
+        } catch (IOException e) {
+            System.err.println("Error reading file: " + e.getMessage());
+            return null;
+        }
+        return content.toString();
+    }
+
+    private static void writeFile(String filePath, String content) {
+        try (FileWriter writer = new FileWriter(filePath)) {
+            writer.write(content);
+            System.out.println("Encoded text written to file: " + filePath);
+        } catch (IOException e) {
+            System.err.println("Error writing to file: " + e.getMessage());
+        }
+    }
+}
---
+++ src/test/huffmanCoding/HuffmanEncoding.java
@@ -0,0 +1,162 @@
+package test.huffmanCoding;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+
+public class HuffmanEncoding {
+
+    static class AlpStatus implements Comparable<AlpStatus> {
+        int character, count;
+
+        AlpStatus left, right;
+        int code;
+
+        AlpStatus(int character, int count) {
+            this.character = character;
+            this.count = count;
+            this.left = null;
+            this.right = null;
+        }
+
+        AlpStatus(int count, AlpStatus left, AlpStatus right) {
+            this.character = -1; // parent node
+            this.count = count;
+            this.left = left;
+            this.right = right;
+        }
+
+        public void setCode(int code) {
+            this.code = code;
+        }
+
+        @Override
+        public int compareTo(AlpStatus o) {
+            return this.count - o.count;
+
+        }
+    }
+
+    // 문자에 대한 허프만 코드를 저장할 Map 생성
+    static Map<Character, String> huffmanCodes = new HashMap<>();
+
+    public static void main(String[] args) {
+        String filePath = "/Users/making/IdeaProjects/ps/src/test/huffmanCoding/originalText.txt";  // 텍스트 파일 경로
+        String text = readFile(filePath);
+        if (text == null) {
+            System.out.println("File content is NULL");
+            return;
+        }
+
+        System.out.println(">>>>>>>>>> ORIGINAL TEXT START <<<<<<<<<<");
+        System.out.print(text);
+        System.out.println(">>>>>>>>>>> ORIGINAL TEXT END <<<<<<<<<<<");
+
+        // 허프만 코딩 시작
+        // 알파벳 개수만큼의 배열 선언 (소문자만 존재함을 가정)
+        int[] alpCount = new int[26];
+
+        // 허프만 코딩, 그리디 알고리즘 -> 우선순위큐 (오름차순, 빈도수가 적은 것 부터)
+        PriorityQueue<AlpStatus> pq = new PriorityQueue<>();
+
+        for (int i = 0; i < text.length(); i++) {
+            char currentChar = text.charAt(i);
+
+            // 소문자만 존재함을 가정
+            if (currentChar >= 'a' && currentChar <= 'z') {
+                int diff = currentChar - 'a';
+                alpCount[diff]++;
+            }
+        }
+
+        // 빈도수가 0이 아닌 알파벳을 우선순위큐에 추가
+        for (int i = 0; i < 26; i++) {
+            if (alpCount[i] != 0) {
+                pq.offer(new AlpStatus(i, alpCount[i]));
+            }
+        }
+
+        // 큐가 비어있을 경우 예외 처리
+        if (pq.isEmpty()) {
+            System.out.println("No lowercase alphabet characters in text.");
+            return;
+        }
+
+        while (pq.size() >= 2) {
+            AlpStatus firstChild = pq.poll();
+            AlpStatus secondChild = pq.poll();
+
+            AlpStatus parent = new AlpStatus(firstChild.count + secondChild.count, firstChild, secondChild);
+            pq.offer(parent);
+        }
+
+        // 최종 루트 노드 (허프만 트리의 루트)
+        AlpStatus root = pq.poll();
+
+
+        // 루트부터 허프만 코드 구하기
+        generateCodes(root, "");
+
+        // 입력 텍스트를 허프만 코드로 암호화
+        StringBuilder encodedText = new StringBuilder();
+        for (char c : text.toCharArray()) {
+            if (c >= 'a' && c <= 'z') {
+                encodedText.append(huffmanCodes.get(c));
+            }
+        }
+
+        String content =
+                "header = " + huffmanCodes +
+                "\n" +
+                "encodedText = " + encodedText;
+
+
+        // 인코딩된 텍스트를 파일에 작성
+        String outputFilePath = "/Users/making/IdeaProjects/ps/src/test/huffmanCoding/encodedText.txt"; // 출력 파일 경로 설정
+        writeFile(outputFilePath, content);
+
+    }
+
+    private static void generateCodes(AlpStatus node, String code) {
+        if (node == null) {
+            return;
+        }
+
+        // 리프 노드인 경우 코드 저장
+        if (node.left == null && node.right == null) {
+            huffmanCodes.put((char) ('a' + node.character), code);
+        }
+
+        generateCodes(node.left, code + "0");
+        generateCodes(node.right, code + "1");
+
+    }
+
+
+    private static String readFile(String filePath) {
+        StringBuilder content = new StringBuilder();
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                content.append(line).append("\n");
+            }
+        } catch (IOException e) {
+            System.err.println("Error reading file: " + e.getMessage());
+            return null;
+        }
+        return content.toString();
+    }
+
+    private static void writeFile(String filePath, String content) {
+        try (FileWriter writer = new FileWriter(filePath)) {
+            writer.write(content);
+            System.out.println("Encoded text written to file: " + filePath);
+        } catch (IOException e) {
+            System.err.println("Error writing to file: " + e.getMessage());
+        }
+    }
+}
