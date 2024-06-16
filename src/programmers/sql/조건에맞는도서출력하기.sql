8bcf5bdcecf636218e0770df4b9b575e56776d33
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-06-16 23:04:36 +0900

    [P 조건에맞는도서출력하기]
    SELECT DATE_FORMAT(t.COL, '%y-%m-%d') AS COL

---
+++ src/programmers/sql/조건에맞는도서출력하기.sql
@@ -0,0 +1,19 @@
+-- 코드를 입력하세요
+-- USED_GOODS_BOARD와 USED_GOODS_REPLY 테이블에서 2022년 10월에 작성된 게시글 제목, 게시글 ID, 댓글 ID, 댓글 작성자 ID, 댓글 내용, 댓글 작성일을 조회하는 SQL문을 작성해주세요. 결과는 댓글 작성일을 기준으로 오름차순 정렬해주시고, 댓글 작성일이 같다면 게시글 제목을 기준으로 오름차순 정렬해주세요.
+SELECT
+    b.TITLE,
+    b.BOARD_ID,
+    r.REPLY_ID,
+    r.WRITER_ID,
+    r.CONTENTS,
+    DATE_FORMAT(r.CREATED_DATE, '%Y-%m-%d') AS CREATED_DATE
+FROM
+    USED_GOODS_BOARD AS b
+        JOIN
+    USED_GOODS_REPLY AS r ON b.BOARD_ID = r.BOARD_ID
+WHERE
+    b.CREATED_DATE LIKE '2022-10%'
+ORDER
+    BY
+    r.CREATED_DATE ASC,
+    b.TITLE ASC;
