6ef4956473d6c86e111b78da74ff5a419ff2afe7
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-06-28 04:02:27 +0900

    [BJ 2447] 분할정복
    별 찍기 true <-> false 바꿔도 별 차이없음

--- src/baekJoon/BJ2447.java
+++ src/baekJoon/BJ2447.java
@@ -13,15 +13,15 @@ public class BJ2447 {
 
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(bufferedReader.readLine());
-        map = new boolean[n + 1][n + 1];
+        map = new boolean[n][n];
 
-        drawMap(n, 1, 1);
+        drawMap(n, 0, 0);
 
         StringBuilder stringBuilder = new StringBuilder();
 
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
-                stringBuilder.append(map[i][j] ? "*" : " ");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                stringBuilder.append(map[i][j] ? " " : "*");
             }
             stringBuilder.append("\n");
         }
@@ -33,18 +33,20 @@ public class BJ2447 {
     private static void drawMap(int n, int x, int y) {
 
         if (n == 1) {
-            map[x][y] = true;
             return;
         }
 
         int count = 0;
         for (int i = x; i < x + n; i += n / 3) {
             for (int j = y; j < y + n; j += n / 3) {
-                count++;
-                if (count == 5) {
-                    continue; // 0
+                if (++count == 5) {
+                    for (int si = i; si < i + n / 3; si++) {
+                        for (int sj = j; sj < j + n / 3; sj++) {
+                            map[si][sj] = true;
+                        }
+                    }
                 }
-                drawMap(n / 3, i, j); // 1
+                drawMap(n / 3, i, j); // 분할
             }
         }
     }
