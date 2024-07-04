4ee31dd25a2ae24beede75844c2d1710d9aa5ff1
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-07-04 23:29:13 +0900

    [BJ 2042] 세그먼트 트리, 구간 합

--- src/baekJoon/BJ2042.java
+++ src/baekJoon/BJ2042.java
@@ -51,13 +51,13 @@ public class BJ2042 {
     }
 
     private static void changeNode(int b, long c) {
-        int targetIdx = leafNodeIdx[b];
-        long targetValue = segTree[targetIdx];
-
-        long diff = targetValue - c;
+        int targetIdx = leafNodeIdx[b]; // 바뀌어야할 노드의 인덱스 (b번째 리프 노드)
+        long targetValue = segTree[targetIdx]; // 그 노드가 가진 값
         segTree[targetIdx] = c; // 리프 노드 변경
+
+        long diff = targetValue - c; // 기존 값에서 얼마나 차이가 나는지
         for (targetIdx /= 2; targetIdx > 0; targetIdx /= 2) {
-            segTree[targetIdx] -= diff; // 부모 노드 변경
+            segTree[targetIdx] -= diff; // 그 차이만큼 부모 노드 변경
         }
     }
 
@@ -69,16 +69,19 @@ public class BJ2042 {
     private static long calcSum(int start, int end, int left, int right, int nodeIdx) {
         // 현재 노드의 범위가 구간 합 범위에 완전히 속하지 않는다면
         if (left > end || right < start) {
-            return 0;
+            return 0; // 그 구간의 합은 0
         }
 
         // 현재 노드의 범위가 구간 합 범위에 완전히 속한다면
         if (left >= start && right <= end) {
-            return segTree[nodeIdx];
+            return segTree[nodeIdx]; // 현재 노드 값 반환(포함), 현재 노드는 현재 노드의 서브 트리의 리프 노드의 합
         }
 
         int mid = (left + right) / 2;
+
+        // 세그먼트 트리의 현재 노드 왼쪽 자식 탐색
         long leftSum = calcSum(start, end, left, mid, nodeIdx * 2);
+        // 세그먼트 트리의 현재 노드 오른쪽 자식 탐색
         long rightSum = calcSum(start, end, mid + 1, right, nodeIdx * 2 + 1);
 
         return leftSum + rightSum;
@@ -86,17 +89,24 @@ public class BJ2042 {
 
 
     private static void initSegTree(int nodeIdx, int start, int end) {
+        // 리프 노드
         if (start == end) {
             segTree[nodeIdx] = numbers[start];
-            leafNodeIdx[start] = nodeIdx;
+            leafNodeIdx[start] = nodeIdx; // 세그먼트 트리의 리프 노드 인덱스를 따로 저장
             return;
         }
 
         int midIdx = (start + end) / 2;
 
-        initSegTree(nodeIdx * 2, start, midIdx); // leftChild
-        initSegTree(nodeIdx * 2 + 1, midIdx+1, end); // rightChild
-        segTree[nodeIdx] = segTree[nodeIdx * 2] + segTree[nodeIdx * 2 + 1]; // 좌우 자식 노드 값을 더하여 부모 노드 값 갱신
+        // 왼쪽 자식
+        initSegTree(nodeIdx * 2, start, midIdx);
+
+        // 오른쪽 자식
+        initSegTree(nodeIdx * 2 + 1, midIdx+1, end);
+
+        // 좌우 자식 노드 값을 더하여 부모 노드 값 갱신
+        // 부모 노드 값 = 왼쪽 자식 값 + 오른쪽 자식 값
+        segTree[nodeIdx] = segTree[nodeIdx * 2] + segTree[nodeIdx * 2 + 1];
 
     }
 }
