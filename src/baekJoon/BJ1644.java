5601ae45535b969127eddc27c2d90bd12c0ce110
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-07-07 17:36:36 +0900

    [BJ 1644] 소수, 에라토스테네스의 채, 투 포인터, 연속합
    주석 추가

--- src/baekJoon/BJ1644.java
+++ src/baekJoon/BJ1644.java
@@ -17,29 +17,41 @@ public class BJ1644 {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(bufferedReader.readLine());
 
+        // true : 소수가 아님
+        // false : 소수 (기본값)
         boolean[] isntPrime = new boolean[n + 1];
+
+        // 0, 1은 소수가 아님
         isntPrime[0] = isntPrime[1] = true;
 
+        // i를 2부터 √n까지 반복
         for (int i = 2; i * i <= n; i++) {
+            // i가 소수(혹은 기본값)라면
             if (!isntPrime[i]) {
+                // i의 배수는 모두 소수가 아니다
+                // j를 i*i부터 시작하여 i씩 증가시키며 소수가 아니라고 표시
                 for (int j = i * i; j <= n; j += i) {
                     isntPrime[j] = true;
                 }
             }
         }
 
+        // 2는 소수다.
         primes.add(2);
+
+        // 3부터 홀수를 체크하며 소수 리스트를 채운다
+        // 간단히 연속합을 구하기 위해서
         for (int i = 3; i <= n; i += 2) {
             if (!isntPrime[i]) {
                 primes.add(i);
             }
         }
 
-        for (int prime : primes) {
-            System.out.println(prime);
-        }
+//        for (int prime : primes) {
+//            System.out.println(prime);
+//        }
 
-
+        // 0(start)번째 소수부터 0(end)번째 소수까지 더한다.
         calcContinuousSum(0, 0);
 
         System.out.println(count);
@@ -49,18 +61,26 @@ public class BJ1644 {
     private static void calcContinuousSum(int start, int end) {
         int sum = 0;
 
-        System.out.println("primes = " + primes.size());
+//        System.out.println("primes = " + primes.size());
 
+        // end 가 소수 리스트의 끝을 넘어서지 않았다면
         while (end < primes.size()) {
 
+            // 소수의 연속합이 목표보다 작다면
+            // 연속합에 end를 더하고 end++
             if (sum < n) {
                 sum += primes.get(end++);
                 continue;
             }
+            // 소수의 연속합이 목표보다 크다면
+            // 연속합에 start를 뺴고 start++
             if (sum > n) {
                 sum -= primes.get(start++);
                 continue;
             }
+            // 소수의 연속합이 목표와 같다면
+            // 연속합에 end를 더하고 end++ (다음 연속합도 구하기 위해서)
+            // count(정답)++
             if (sum == n) {
                 count++;
                 sum += primes.get(end++);
@@ -68,6 +88,9 @@ public class BJ1644 {
             }
         }
 
+        // 위 반복을 완료한다면
+        // 소수 리스트의 마지막과 목표값이 같은지 비교
+        // end++를 해서 마지막 while문의 조건에 만족하지 못해서 예외처리..
         if (n == primes.get(primes.size() - 1)) {
             count++;
         }
