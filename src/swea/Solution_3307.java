beaadd7c8af481baed476003a5e239c9f736d5ae
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-07-23 11:37:55 +0900

    [SW 3307] LIS LIS 요소를 출력하는 것보다 간단 이진탐색 로워바운드

---
+++ src/swea/Solution_3307.java
@@ -0,0 +1,56 @@
+package swea;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution_3307 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(bufferedReader.readLine());
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (int t = 0; t < tc; t++) {
+            int n = Integer.parseInt(bufferedReader.readLine());
+            StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine(), " ");
+
+            int[] LIS = new int[n + 1];
+            int tailIdx = 0;
+//            System.out.println();
+            for (int i = 0; i < n; i++) {
+//
+//                for (int lis : LIS) {
+//                    System.out.print(lis + " ");
+//                }
+//                System.out.println();
+
+                int currNum = Integer.parseInt(stringTokenizer.nextToken());
+                if (LIS[tailIdx] < currNum) {
+                    LIS[++tailIdx] = currNum;
+                } else {
+                    int start = 0;
+                    int end = tailIdx;
+                    int mid = 0;
+                    while (start < end) {
+                        mid = (start + end) / 2;
+
+                        if (currNum > LIS[mid]) {
+                            start = mid + 1;
+                        } else {
+                            end = mid;
+                        }
+                    }
+
+                    LIS[end] = currNum;
+                }
+            }
+
+            stringBuilder.append("#").append(t + 1).append(" ").append(tailIdx).append("\n");
+        }
+        System.out.println(stringBuilder);
+
+    }
+}
