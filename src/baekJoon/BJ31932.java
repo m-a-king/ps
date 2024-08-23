05d9998bb7244b4b507b24a096615ca64f41f524
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-08-23 16:47:13 +0900

    [BJ 31932] 북극곰 문제 역방향 탐색
    이분탐색을 활용한 정방향 탐색과 다르게 섬세한 조건 처리가 필요했다. 큐를 효율적으로 활용해야지만 풀 수 있음

--- src/baekJoon/BJ31932.java
+++ src/baekJoon/BJ31932.java
@@ -9,33 +9,32 @@ public class BJ31932 {
 
     private static class Edge {
         int to;
-        long dist, breakTime;
+        int dist, breakTime;
 
-        public Edge(int to, long dist, long breakTime) {
+        public Edge(int to, int dist, int breakTime) {
             this.to = to;
             this.dist = dist;
             this.breakTime = breakTime;
         }
     }
 
-    private static class Reverser implements Comparable<Reverser> {
-        int pos;
-        long time;
+    private static class Node implements Comparable<Node> {
+        int idx;
+        int deadLine;
 
-        public Reverser(int pos, long time) {
-            this.pos = pos;
-            this.time = time;
+        public Node(int idx, int deadLine) {
+            this.idx = idx;
+            this.deadLine = deadLine;
         }
 
         @Override
-        public int compareTo(Reverser other) {
-            return Long.compare(other.time, this.time); // 큰 시간이 우선이 되도록 정렬
+        public int compareTo(Node other) {
+            return other.deadLine - this.deadLine;
         }
     }
 
     static int n, m;
     static List<List<Edge>> graph;
-    static long[] bestRecord;
 
     public static void main(String[] args) throws IOException {
 
@@ -50,51 +49,54 @@ public class BJ31932 {
             graph.add(new ArrayList<>());
         }
 
-        bestRecord = new long[n + 1];  // 도착지(출발하는 지점)에서 각 정점까지의 이동했을 때 가장 많이 남은 시간
-        Arrays.fill(bestRecord, 0);
-
         for (int i = 1; i <= m; i++) {
             stringTokenizer = new StringTokenizer(bufferedReader.readLine(), " ");
             int u = Integer.parseInt(stringTokenizer.nextToken());
             int v = Integer.parseInt(stringTokenizer.nextToken());
-            long d = Long.parseLong(stringTokenizer.nextToken());
-            long t = Long.parseLong(stringTokenizer.nextToken());
+            int d = Integer.parseInt(stringTokenizer.nextToken());
+            int t = Integer.parseInt(stringTokenizer.nextToken());
+
+            if (d > t) continue;
 
             graph.get(u).add(new Edge(v, d, t));
             graph.get(v).add(new Edge(u, d, t));
         }
 
-        PriorityQueue<Reverser> pq = new PriorityQueue<>();
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        int[] bestDeadLine = new int[n + 1];
+        Arrays.fill(bestDeadLine, -1);
 
-        for (Edge sEdge : graph.get(n)) {
-            pq.offer(new Reverser(sEdge.to, sEdge.breakTime - sEdge.dist));
-            bestRecord[sEdge.to] = sEdge.breakTime - sEdge.dist;
+        for (Edge edge : graph.get(n)) {
+            pq.offer(new Node(edge.to, edge.breakTime - edge.dist));
+            bestDeadLine[n] = Math.max(bestDeadLine[n], edge.breakTime);
+            bestDeadLine[edge.to] = edge.breakTime - edge.dist;
         }
-
-        long maxTime = 0;
-        boolean canArrived = false;
 
         while (!pq.isEmpty()) {
-            Reverser curr = pq.poll();
+            Node curr = pq.poll();
+            if(curr.deadLine < bestDeadLine[curr.idx]) continue;
+            bestDeadLine[curr.idx] = curr.deadLine;
 
-            if (curr.pos == 1) {
-                canArrived = true;
-                maxTime = Math.max(maxTime, curr.time);
+            if (curr.idx == 1) {
+                System.out.println(curr.deadLine);
+                return;
             }
 
-            List<Edge> nextEdges = graph.get(curr.pos);
+            for (Edge adjEdge : graph.get(curr.idx)) {
+                int nextNodeDeadLine = curr.deadLine - adjEdge.dist;
+                if (nextNodeDeadLine < 0) continue;
 
-            for (Edge nextEdge : nextEdges) {
-                long remainTime = curr.time - nextEdge.dist;
-                if (remainTime < 0) continue;
+                if (curr.deadLine > adjEdge.breakTime) {
+                    nextNodeDeadLine = adjEdge.breakTime- adjEdge.dist;
+                }
 
-                if (remainTime < bestRecord[nextEdge.to]) continue;
+                if (nextNodeDeadLine <= bestDeadLine[adjEdge.to]) continue; // 있는게 낫긴 한듯 ?
+                bestDeadLine[adjEdge.to] = nextNodeDeadLine; // 있는게 낫긴 한듯 ?
 
-                bestRecord[nextEdge.to] = remainTime;  // 최적 시간 갱신
-                pq.offer(new Reverser(nextEdge.to, remainTime));
+                pq.offer(new Node(adjEdge.to, nextNodeDeadLine));
             }
         }
 
-        System.out.println(canArrived ? maxTime : -1);
+        System.out.println(-1);
     }
 }
