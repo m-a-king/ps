d0eee0e233bd7bf563e9191d26272677e719306b
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-09-12 20:59:48 +0900

    [BJ 5337]

---
+++ src/baekJoon/BJ5337.java
@@ -0,0 +1,15 @@
+package baekJoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BJ5337 {
+
+    public static void main(String[] args) {
+
+
+        System.out.println(".  .   .\n" +
+                "|  | _ | _. _ ._ _  _\n" +
+                "|/\\|(/.|(_.(_)[ | )(/.");
+    }
+}
