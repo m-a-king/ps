2c3418a47127527ebf853f95772cb77afcea6066
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-09-17 18:20:04 +0900

    [BJ 5339]

---
+++ src/baekJoon/BJ5339.java
@@ -0,0 +1,18 @@
+package baekJoon;
+
+public class BJ5339 {
+
+    public static void main(String[] args) {
+
+        System.out.println("     /~\\\n" +
+                "    ( oo|\n" +
+                "    _\\=/_\n" +
+                "   /  _  \\\n" +
+                "  //|/.\\|\\\\\n" +
+                " ||  \\ /  ||\n" +
+                "============\n" +
+                "|          |\n" +
+                "|          |\n" +
+                "|          |");
+    }
+}
