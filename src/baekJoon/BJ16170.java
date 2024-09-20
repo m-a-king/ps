30029a8f1db426c3c29208a6975a0856e0fa5766
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-09-20 19:58:34 +0900

    [BJ 16170]

---
+++ src/baekJoon/BJ16170.java
@@ -0,0 +1,14 @@
+package baekJoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BJ16170 {
+
+    public static void main(String[] args) {
+
+        System.out.println("2024");
+        System.out.println("09");
+        System.out.println("20");
+    }
+}
