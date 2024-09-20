e98a50f75883db8dd627ffd092c76a25dd07bc8d
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-09-20 22:48:46 +0900

    [P 양과늑대] 풀이 성공 최적화 전

--- src/programmers/양과늑대.java
+++ src/programmers/양과늑대.java
@@ -6,15 +6,15 @@ public class 양과늑대 {
 
     public static void main(String[] args) {
 
-//        int[] info = {0, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1};
-//        int[][] edges = {{0, 1}, {1, 2}, {1, 4}, {0, 8}, {8, 7}, {9, 10}, {9, 11}, {4, 3}, {6, 5}, {4, 6}, {8, 9}};
+        int[] info = {0, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1};
+        int[][] edges = {{0, 1}, {1, 2}, {1, 4}, {0, 8}, {8, 7}, {9, 10}, {9, 11}, {4, 3}, {6, 5}, {4, 6}, {8, 9}};
+
+        System.out.println(solution(info, edges));
+
+//        int[] info2 = {0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0};
+//        int[][] edges2 = {{0, 1}, {0, 2}, {1, 3}, {1, 4}, {2, 5}, {2, 6}, {3, 7}, {4, 8}, {6, 9}, {9, 10}};
 //
-//        System.out.println(solution(info, edges));
-
-        int[] info2 = {0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0};
-        int[][] edges2 = {{0, 1}, {0, 2}, {1, 3}, {1, 4}, {2, 5}, {2, 6}, {3, 7}, {4, 8}, {6, 9}, {9, 10}};
-
-        System.out.println(solution(info2, edges2));
+//        System.out.println(">>> answer: " + solution(info2, edges2));
 
     }
 
@@ -27,11 +27,11 @@ public class 양과늑대 {
             this.idx = idx;
             this.passWolfs = passWolfs;
         }
+
     }
 
     static List<List<Integer>> adjList = new ArrayList<>();
     static int[] infos;
-    static int maxSheep = 0;
     static List<Sheep> sheep = new ArrayList<>();
 
     public static int solution(int[] info, int[][] edges) {
@@ -51,40 +51,82 @@ public class 양과늑대 {
 
     }
 
+    static int[] seq;
+    static int sheepCount;
+    static int initSheep = 0;
+    static boolean[] visited;
+
     private static int countMaxSheep() {
-        Set<Integer> accumulatedWolf = new HashSet<>();
-        int sheepCount = 0;
+        sheepCount = 0;
+        List<Sheep> temp = new ArrayList<>();
 
-        while (true) {
-            int currSheepCount = sheepCount;
+        for (Sheep s : sheep) {
+            if (s.passWolfs.isEmpty()) {
+                sheepCount++;
+                continue;
+            }
+            temp.add(s);
+        }
 
-            for (Sheep s : sheep) {
+        initSheep = sheepCount;
+        sheep = temp;
+        seq = new int[sheep.size()];
+        visited = new boolean[sheep.size()];
 
-                if(s.visited) continue;
+        for (int i = 0; i < sheep.size(); i++) {
+            if (visited[i]) continue;
+
+            visited[i] = true;
+            seq[0] = i;
+
+            permutation(1);
+
+            visited[i] = false;
+        }
+
+        return sheepCount;
+    }
+
+    static Set<Integer> accumulatedWolf;
+
+    private static void permutation(int depth) {
+        if (depth == sheep.size()) {
+            int newSheepCount = initSheep;
+            accumulatedWolf = new HashSet<>();
+
+            for (int i = 0; i < depth; i++) {
+                Sheep curr = sheep.get(seq[i]);
 
                 Set<Integer> temp = new HashSet<>(accumulatedWolf);
-                temp.addAll(s.passWolfs);
 
-                if (s.passWolfs.isEmpty() || temp.size() < sheepCount) {
-                    sheepCount++;
-                    s.visited = true;
-                    System.out.println(s.idx);
+                temp.addAll(curr.passWolfs);
+
+                if (temp.size() < newSheepCount) {
                     accumulatedWolf = temp;
+                    newSheepCount++;
                 }
             }
 
-            if (currSheepCount == sheepCount) {
-                return sheepCount;
-            }
+            sheepCount = Math.max(sheepCount, newSheepCount);
+            return;
         }
+
+        for (int i = 0; i < sheep.size(); i++) {
+            if (visited[i]) continue;
+
+            visited[i] = true;
+            seq[depth] = i;
+
+            permutation(depth + 1);
+
+            visited[i] = false;
+        }
+
     }
 
     private static void checkPassWolf(int curr, Deque<Integer> passWolfs) {
         if (infos[curr] == 0) {
             sheep.add(new Sheep(curr, new ArrayList<>(passWolfs)));
-            System.out.println("curr = " + curr);
-            System.out.println(passWolfs.toString());
-            System.out.println();
         }
 
         for (Integer next : adjList.get(curr)) {
@@ -94,30 +136,5 @@ public class 양과늑대 {
             if (flag) passWolfs.pop();
         }
     }
-
 }
 
-//    private static void dfs(int curr) {
-//        sheepWolf[infos[curr]]++;
-//
-//        if (sheepWolf[0] <= sheepWolf[1]) {
-//            sheepWolf[infos[curr]]--;
-//            return;
-//        } else {
-//            maxSheep = Math.max(maxSheep, sheepWolf[0]);
-//        }
-//
-//
-//        for (int next : adjList.get(curr)) {
-//            System.out.println("curr = " + curr);
-//            System.out.println("next = " + next);
-//            System.out.println("sheep = " + sheepWolf[0]);
-//            System.out.println("wolf = " + sheepWolf[1]);
-//            System.out.println();
-//            dfs(next);
-//        }
-//
-//        if(infos[curr] == 1) sheepWolf[infos[curr]]--;
-//
-//    }
-//}
