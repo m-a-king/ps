1a49565fd91391edfdd1ba8aafac826436019533
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-09-28 18:19:30 +0900

    [BJ 27433]

---
+++ src/baekJoon/BJ27323.java
@@ -0,0 +1,18 @@
+package baekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BJ27323 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(bufferedReader.readLine());
+        int b = Integer.parseInt(bufferedReader.readLine());
+
+        System.out.println(a*b);
+
+    }
+}
