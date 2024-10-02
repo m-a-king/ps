96e02dfda1a5b502a36bf8d6ed528985dd516183
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-10-02 23:29:34 +0900

    [BJ 14645] 어이없는 문제 ㅋㅋㅋㅋ

---
+++ src/baekJoon/BJ14645.java
@@ -0,0 +1,13 @@
+package baekJoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BJ14645 {
+
+    public static void main(String[] args) {
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("비와이");
+    }
+}
