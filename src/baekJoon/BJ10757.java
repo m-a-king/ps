28cbcfdb12cb6d19af00c8c188b65685c744542d
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-10-03 20:11:47 +0900

    [BJ 10757]

---
+++ src/baekJoon/BJ10757.java
@@ -0,0 +1,21 @@
+package baekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class BJ10757 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine(), " ");
+
+        BigInteger a = new BigInteger(stringTokenizer.nextToken());
+        BigInteger b = new BigInteger(stringTokenizer.nextToken());
+
+        System.out.println(a.add(b));
+    }
+}
