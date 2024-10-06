8bb5a8bad3f5205a136e87d2651e9bffdeb22357
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-10-06 17:47:35 +0900

    [P 완주하지못한선수]

---
+++ src/programmers/P완주하지못한선수.java
@@ -0,0 +1,24 @@
+package programmers;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class P완주하지못한선수 {
+
+    class Solution {
+        public String solution(String[] participant, String[] completion) {
+            Map<String, Integer> map = new HashMap<>();
+            for(String p : participant){
+                map.compute(p, (k,v) -> (v == null) ? 0 : v+1);
+            }
+
+            for(String c: completion){
+                map.compute(c, (k,v) -> v-1);
+            }
+
+            for(Map.Entry<String, Integer> e : map.entrySet()){
+                if(e.getValue() != 0) return e.getKey();
+            }
+        }
+    }
+}
