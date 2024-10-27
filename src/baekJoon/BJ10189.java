837bd8d5873e1a18be05b48cf1f525791a813035
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-10-27 23:55:52 +0900

    [BJ 10189]

---
+++ src/baekJoon/BJ10189.java
@@ -0,0 +1,13 @@
+package baekJoon;
+
+public class BJ10189 {
+
+    public static void main(String[] args) {
+
+        System.out.println(
+                "#  # #### #### #  #\n" +
+                "#### #  # #  # # #\n" +
+                "#### #  # #  # # #\n" +
+                "#  # #### #### #  #");
+    }
+}
