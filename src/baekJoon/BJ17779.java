4a24553dfb6c72214d0b897dff55be0ae078ae42
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-11-17 23:07:38 +0900

    [BJ 17779] 재풀이 필요

--- src/baekJoon/BJ17779.java
+++ src/baekJoon/BJ17779.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+// 재채점 오답 문제
+
 public class BJ17779 {
     static int n;
     static int[][] map;
