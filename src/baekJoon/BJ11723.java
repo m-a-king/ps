71727ac4ed5352305b8642f31b27b81e0639a5ab
조재중 <126754298+m-a-king@users.noreply.github.com>
2025-01-12 21:15:18 +0900

    [BJ 11723] 비트마스킹
    - 기존 hashSet 활용 문제를 재풀이

--- src/baekJoon/BJ11723.java
+++ src/baekJoon/BJ11723.java
@@ -3,60 +3,68 @@ package baekJoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashSet;
-import java.util.Set;
-
-import static java.lang.Integer.parseInt;
+import java.util.StringTokenizer;
 
 public class BJ11723 {
+
+    private static int bit = 0;
+    private static StringBuilder stringBuilder = new StringBuilder();
+
     public static void main(String[] args) throws IOException {
 
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        int n = parseInt(bufferedReader.readLine());
+        int tc = Integer.parseInt(bufferedReader.readLine());
 
-        Set<Integer> list = new HashSet<>();
-        StringBuilder sb = new StringBuilder();
+        while (tc-- > 0) {
+            StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine(), " ");
 
-        for (int i = 0; i < n; i++) {
-            String[] input = bufferedReader.readLine().split(" ");
-
-            if (input.length == 2) {
-                String command = input[0];
-                int number = parseInt(input[1]);
-                action(command, number, list, sb);
-            } else {
-                String command = input[0];
-                action(command, -1, list, sb);
-            }
+            process(stringTokenizer);
         }
 
-        System.out.println(sb);
+        System.out.println(stringBuilder);
     }
 
-    private static void action(String command, int n, Set<Integer> list, StringBuilder stringBuilder) {
-        if (command.equals("add")) {
-            list.add(n);
-        } else if (command.equals("remove")) {
-            list.remove(n);
-        } else if (command.equals("check")) {
-            if (list.contains(n)) {
-                stringBuilder.append(1 + "\n");
-            } else {
-                stringBuilder.append(0 + "\n");
-            }
-        } else if (command.equals("toggle")) {
-            if (list.contains(n)) {
-                list.remove(n);
-            } else {
-                list.add(n);
-            }
-        } else if (command.equals("all")) {
-            list.clear();
-            for (int i = 1; i <= 20; i++) {
-                list.add(i);
-            }
-        } else if (command.equals("empty")) {
-            list.clear();
+    private static void process(StringTokenizer stringTokenizer) {
+        String cmd = stringTokenizer.nextToken();
+
+        if (cmd.equals("all")) {
+            bit |= (1 << 21) - 1;
+            return;
         }
+
+        if (cmd.equals("empty")) {
+            bit = 0;
+            return;
+        }
+
+        int x = Integer.parseInt(stringTokenizer.nextToken());
+        int temp = 1 << x;
+
+
+        if (cmd.equals("add")) {
+            bit |= temp;
+            return;
+        }
+
+        if (cmd.equals("remove")) {
+            bit &= ~temp;
+            return;
+        }
+
+        if (cmd.equals("check")) {
+            if ((bit & temp) == temp) {
+                stringBuilder.append(1);
+            } else {
+                stringBuilder.append(0);
+            }
+            stringBuilder.append("\n");
+            return;
+        }
+
+        if (cmd.equals("toggle")) {
+            bit ^= temp;
+            return;
+        }
+
     }
 }
