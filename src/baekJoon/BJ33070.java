100d9d8a3759cf328027d0fdb6d8dd78610f2e46
조재중 <126754298+m-a-king@users.noreply.github.com>
2025-04-09 23:39:22 +0900

    [BJ 33070] 세그먼트 트리 실패

--- src/baekJoon/BJ33070.java
+++ src/baekJoon/BJ33070.java
@@ -3,135 +3,154 @@ package baekJoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.HashSet;
 import java.util.StringTokenizer;
-import java.util.stream.Collectors;
 
 public class BJ33070 {
+    static int N, K;
+    static boolean[] possible;
+    static int[] base;
+    static int[] init;
 
-    private static class SegmentTree {
-        int[] tree;
-        int n;
-        int value;
-
-        public SegmentTree(int[] origin, int value) {
-            this.n = origin.length - 1;
-            this.value = value;
-            this.tree = new int[4 * n];
-            build(origin, 1, 1, n);
-        }
-
-        public int query(int right) {
-            return query(1, 1, n, 1, right);
-        }
-
-        public void add(int target) {
-            update(1, 1, n, target);
-        }
-
-        private void build(int[] origin, int node, int start, int end) {
-            if (isLeaf(start, end)) {
-                tree[node] = origin[start];
-                return;
-            }
-
-            int mid = calcMid(start, end);
-            build(origin, node * 2, start, mid);
-            build(origin, node * 2 + 1, mid + 1, end);
-
-            calcParent(node);
-        }
-
-        private int query(int node, int start, int end, int left, int right) {
-            if (left > end || right < start) {
-                return 0;
-            }
-
-            if (left <= start && end <= right) {
-                return tree[node];
-            }
-
-            int mid = calcMid(start, end);
-            int leftSum = query(node * 2, start, mid, left, right);
-            int rightSum = query(node * 2 + 1, mid + 1, end, left, right);
-            return leftSum + rightSum;
-        }
-
-        private void update(int node, int start, int end, int target) {
-            if (isLeaf(start, end)) {
-                tree[node] += value;
-                return;
-            }
-
-            int mid = calcMid(start, end);
-            if (target <= mid) {
-                update(node * 2, start, mid, target);
-            } else {
-                update(node * 2 + 1, mid + 1, end, target);
-            }
-
-            calcParent(node);
-        }
-
-        private static int calcMid(int start, int end) {
-            return (start + end) / 2;
-        }
-
-
-        private static boolean isLeaf(int start, int end) {
-            return start == end;
-        }
-
-        private void calcParent(int node) {
-            tree[node] = tree[node * 2] + tree[node * 2 + 1];
-        }
-    }
+    static SegTree seg;
 
     public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
 
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-        int N = Integer.parseInt(stringTokenizer.nextToken());
-        int K = Integer.parseInt(stringTokenizer.nextToken());
-
-        int[] A = new int[K];
-        stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        possible = new boolean[N + 10]; // 여유 있게
+        st = new StringTokenizer(br.readLine());
         for (int i = 0; i < K; i++) {
-            A[i] = Integer.parseInt(stringTokenizer.nextToken());
-        }
-        Arrays.sort(A);
-        HashSet<Integer> searchableA = Arrays.stream(A)
-                .boxed()
-                .collect(Collectors.toCollection(HashSet::new));
-
-
-        int[] light = new int[N + 1];
-        stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-        for (int i = 1; i <= N; i++) {
-            light[i] = stringTokenizer.nextToken().equals("1") ? 0 : 1;
-        }
-
-        SegmentTree segmentTree = new SegmentTree(light, 1);
-        int totalOff = segmentTree.query(N);
-
-        boolean updated = true;
-        while (updated) {
-            updated = false;
-            for (int i = N; i > 0; i--) {
-                if (light[i] == 1) continue;
-
-                int offCount = segmentTree.query(i - 1);
-                if (searchableA.contains(offCount)) {
-                    light[i] = 1;
-                    segmentTree.add(i);
-                    totalOff++;
-                    updated = true;
-                    break;
-                }
+            int a = Integer.parseInt(st.nextToken());
+            if (a < possible.length) {
+                possible[a] = true;
             }
         }
 
-        System.out.println(N - totalOff);
+        init = new int[N + 1];
+        base = new int[N + 1];
+        int cntOff = 0;
+        int onCount = 0;
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            int state = Integer.parseInt(st.nextToken());
+            init[i] = state;
+            if (state == 0) {
+                cntOff++;
+                base[i] = -1;
+            } else {
+                base[i] = cntOff;
+                onCount++;
+            }
+        }
+
+        seg = new SegTree(N);
+        seg.build(1, 1, N);
+
+        int additionalOff = 0; // 추가로 소등한 on 방 수
+
+        while (true) {
+            int idx = seg.query(1, 1, N);
+            if (idx == -1) break;
+            seg.updatePoint(1, 1, N, idx); // idx번 방 소등 처리
+            additionalOff++;
+            if (idx < N)
+                seg.updateRange(1, 1, N, idx + 1, N, 1);
+        }
+
+        int answer = onCount - additionalOff;
+        System.out.println(answer);
+    }
+
+    static class SegTree {
+        int n;
+        int[] tree;
+        int[] lazy;
+        int[] eff;
+
+        public SegTree(int n) {
+            this.n = n;
+            tree = new int[4 * n];
+            lazy = new int[4 * n];
+            eff = new int[n + 1]; // 1-indexed
+        }
+
+        int calcLeaf(int i) {
+            if (init[i] == 0) return -1;
+            return (possible[eff[i]] ? i : -1);
+        }
+
+        void pushUp(int node) {
+            tree[node] = Math.max(tree[node * 2], tree[node * 2 + 1]);
+        }
+
+        void pushDown(int node, int l, int r) {
+            if (lazy[node] != 0) {
+                int mid = (l + r) >> 1;
+                updateNode(node * 2, l, mid, lazy[node]);
+                updateNode(node * 2 + 1, mid + 1, r, lazy[node]);
+                lazy[node] = 0;
+            }
+        }
+
+        void updateNode(int node, int l, int r, int delta) {
+            lazy[node] += delta;
+            if (l == r) {
+                eff[l] += delta;
+                tree[node] = calcLeaf(l);
+            }
+        }
+
+        void updateRange(int node, int l, int r, int ql, int qr, int delta) {
+            if (ql <= l && r <= qr) {
+                updateNode(node, l, r, delta);
+                return;
+            }
+            pushDown(node, l, r);
+            int mid = (l + r) >> 1;
+            if (ql <= mid) updateRange(node * 2, l, mid, ql, qr, delta);
+            if (qr > mid) updateRange(node * 2 + 1, mid + 1, r, ql, qr, delta);
+            pushUp(node);
+        }
+
+        void updatePoint(int node, int l, int r, int pos) {
+            if (l == r) {
+                init[pos] = 0;
+                tree[node] = -1;
+                return;
+            }
+            pushDown(node, l, r);
+            int mid = (l + r) >> 1;
+            if (pos <= mid) updatePoint(node * 2, l, mid, pos);
+            else updatePoint(node * 2 + 1, mid + 1, r, pos);
+            pushUp(node);
+        }
+
+        void build(int node, int l, int r) {
+            if (l == r) {
+                if (init[l] == 1)
+                    eff[l] = base[l];
+                else
+                    eff[l] = -1000000000;
+                tree[node] = calcLeaf(l);
+                return;
+            }
+            int mid = (l + r) >> 1;
+            build(node * 2, l, mid);
+            build(node * 2 + 1, mid + 1, r);
+            pushUp(node);
+        }
+
+        int query(int node, int l, int r) {
+            if (l == r)
+                return calcLeaf(l);
+            pushDown(node, l, r);
+            int mid = (l + r) >> 1;
+            int rightCandidate = query(node * 2 + 1, mid + 1, r);
+            if (rightCandidate != -1)
+                return rightCandidate;
+            return query(node * 2, l, mid);
+        }
     }
 }
