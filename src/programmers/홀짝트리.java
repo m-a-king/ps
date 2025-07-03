f058ebe040dd5826eb5ec76ef79ecc52ec66515d
조재중 <126754298+m-a-king@users.noreply.github.com>
2025-07-03 22:31:18 +0900

    [P 홀짝트리]

---
+++ src/programmers/홀짝트리.java
@@ -0,0 +1,15 @@
+package programmers;
+
+public class 홀짝트리 {
+
+    static class Solution {
+        public int[] solution(int[] nodes, int[][] edges) {
+            int[] answer = {};
+
+            
+
+            return answer;
+        }
+    }
+
+}
