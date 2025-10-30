bbb81771a6c43d1e71c5515244139c6e44d86492
조재중 <126754298+m-a-king@users.noreply.github.com>
2025-10-30 21:47:09 +0900

    [P 네트워크 개선]
    - 불필요한 리스트 삭제.

--- src/programmers/네트워크.java
+++ src/programmers/네트워크.java
@@ -9,47 +9,29 @@ class 네트워크 {
     public int solution(int n, int[][] computers) {
         int answer = 0;
 
-        List<List<Integer>> graph = new ArrayList<>();
-
-        for (int i = 0; i < n; i++) {
-            graph.add(new ArrayList<>());
-        }
-
-        for (int i = 0; i < n; i++) {
-            for (int j = i+1; j < n; j++) {
-                if (computers[i][j] == 1) {
-                    graph.get(i).add(j);
-                    graph.get(j).add(i);
-                }
-            }
-        }
-
-        Queue<Integer> queue = new ArrayDeque<>();
         boolean[] visited = new boolean[n];
+        Queue<Integer> queue = new ArrayDeque();
 
-        for (int i = 0; i < n; i++) {
+        for(int i=0;i<n;i++) {
+            if(visited[i]) continue;
+            visited[i] = true;
 
-            if (!visited[i]) {
-                queue.offer(i);
-                visited[i] = true;
-                bfs(queue, graph, visited);
-                answer++;
-            }
-        }
-        return answer;
-    }
+            queue.offer(i);
+            answer++;
 
-    private void bfs(Queue<Integer> queue, List<List<Integer>> graph, boolean[] visited) {
-        while (!queue.isEmpty()) {
-            int current = queue.poll();
+            while(!queue.isEmpty()) {
+                int curr = queue.poll();
 
-            List<Integer> nextNodes = graph.get(current);
-            for (int next : nextNodes) {
-                if (!visited[next]) {
-                    visited[next] = true;
-                    queue.offer(next);
+                for(int j=0;j<n;j++){
+                    if(j==curr) continue;
+                    if(visited[j]) continue;
+                    if(computers[curr][j] == 0) continue;
+                    visited[j] = true;
+                    queue.offer(j);
                 }
             }
         }
+
+        return answer;
     }
 }
